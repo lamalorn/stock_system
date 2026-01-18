11b301b166fc8a04f77f94431a5597276b8b3efd
lamalorn <lamalorn1@gmail.com>
2026-01-19 00:37:51 +0700

    done crude supplier

--- database/migrations/200_ purchasing/0201_create_suppliers_table.php
+++ database/migrations/200_ purchasing/0201_create_suppliers_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('address')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
